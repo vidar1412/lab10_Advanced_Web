7659cc12da0a6e58b09d41ab514207713434bc25
vidar1412 <vidar1412@gmail.com>
2022-05-10 22:54:06 +0700

    Fixing routes problems

--- app.js
+++ app.js
@@ -25,6 +25,9 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
+
 app.use(
   session({
     resave: false,
@@ -36,9 +39,6 @@ app.use(
 app.use(passport.initialize());
 app.use(passport.session());
 
-
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
